0ab30a735fe5869195032bf7e6117f3e77083be1
iindewianjani <43032531+iindewianjani@users.noreply.github.com>
2018-09-07 10:54:22 +0700

    Add files via upload

---
+++ Iin.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication1;
+
+/**
+ *
+ * @author anjani
+ */
+public class Iin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int jumlah=5;
+        for (int i=1;i<=jumlah;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+    
+
+
